83d67f77cef21271138b931ff182cb2b9c9a9532
mouravocal <diego.moura3@gmail.com>
2024-08-01 16:33:53 -0300

    add zinit plugins

--- zinit-install.sh
+++ zinit-install.sh
@@ -5,4 +5,14 @@ bash -c "$(curl --fail --show-error --silent --location https://raw.githubuserco
 
 zsh && zinit self-update
 
+cat <<EOF >> ~/.zshrc
+# Plugin history-search-multi-word loaded with investigating.
+zinit load zdharma-continuum/history-search-multi-word
+# Two regular plugins loaded without investigating.
+zinit light zsh-users/zsh-autosuggestions
+zinit light zdharma-continuum/fast-syntax-highlighting
+# Snippet
+zinit snippet https://gist.githubusercontent.com/hightemp/5071909/raw/
+EOF
+
 echo "Reinicie o seu Ubuntu caso o shell não inicie com zsh"
