a533c7a0ee4a0ff1c678f3b2ef042af4fd7f2bc9
mouravocal <diego.moura3@gmail.com>
2024-08-09 09:35:00 -0300

    fix: powerlevel10k

--- powerlevel10k-zinit.sh
+++ powerlevel10k-zinit.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 if [ $(echo $SHELL) != "/usr/bin/zsh" ]; then
-  chsh $(which zsh)
+  chsh $(which./power  zsh)
 fi
 
-echo zinit ice depth=1; zinit light romkatv/powerlevel10k >> ~/.zshrc
+echo "zinit ice depth=1; zinit light romkatv/powerlevel10k" >> ~/.zshrc
